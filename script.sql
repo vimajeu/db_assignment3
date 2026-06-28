75e9ae8cacbf3acf3a52696d57ed751de2c34b01
vimajeu <vitalina.hatsura.25@kse.org.ua>
2026-06-28 16:05:37 +0300

    calculate_order_total function (task 1)

--- script.sql
+++ script.sql
@@ -1,1 +1,9 @@
-
+--task 1
+create or replace function calculate_order_total(p_order_id int)
+returns numeric
+as $$
+	select
+		coalesce(sum(quantity * price), 0)
+	from order_items
+	where order_id = p_order_id;
+$$ language sql;
